ade19dfdea4795ecfe18174ed081d24baf233bd0
DanIglessias <danewolker@gmail.com>
2025-03-19 22:17:47 -0300

    adição header e implementação reponsividade crua

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
 const Header: React.FC = () => {
-    return (
-        <header>
-        <h1>Header</h1>
-        </header>
-    );
-    };  
+  return (
+    <header className="header">
+      <nav className="header-nav">
+        <ul className="header-menu">
+          <li><Link to="/catalogo">Catálogo</Link></li>
+          <li><Link to="/marcas">Marcas</Link></li>
+          <li><Link to="/quem-somos">Quem Somos?</Link></li>
+          <li><Link to="/duvidas">Dúvidas Frequentes</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
