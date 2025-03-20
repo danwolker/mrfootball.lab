7bd36ba4b41b31fc728a2e37323bd96d91ac9d77
DanIglessias <danewolker@gmail.com>
2025-03-19 21:06:03 -0300

    integração header

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 import "../styles/Home.css";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Header />
       <div className="Home">
         <h1>Bem-vindo ao Mr Football Lab</h1>
         <p>Seu e-commerce especializado em chuteiras de futebol.</p>
