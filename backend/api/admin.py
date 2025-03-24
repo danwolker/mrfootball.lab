f31b777a464d462578f7bc50553d9c749c630f97
Romeu <romeufftt@gmail.con>
2025-03-24 11:02:09 -0300

    criando tabela SoccerBoots e ajustes

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile,Addres,BootType,Brand,Color,Order,NewsLetter
+from .models import Profile,Addres,BootType,Brand,Color,Order,NewsLetter,SoccerBoot,Line
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Addres)
@@ -8,3 +8,5 @@ admin.site.register(Brand)
 admin.site.register(Color)
 admin.site.register(Order)
 admin.site.register(NewsLetter)
+admin.site.register(SoccerBoot)
+admin.site.register(Line)
