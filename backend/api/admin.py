7489de0b97d7aa81ab16f29f47433178cd047836
Romeu <romeufftt@gmail.con>
2025-03-24 09:28:24 -0300

    registrando tabela news no admin do django

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile,Addres,BootType,Brand,Color,Order
+from .models import Profile,Addres,BootType,Brand,Color,Order,NewsLetter
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Addres)
@@ -7,3 +7,4 @@ admin.site.register(BootType)
 admin.site.register(Brand)
 admin.site.register(Color)
 admin.site.register(Order)
+admin.site.register(NewsLetter)
