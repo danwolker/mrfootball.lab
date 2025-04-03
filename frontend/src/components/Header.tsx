eb6716357a0703344ba0df7c9ed9bb973ed8ca29
romeu <romeufftt@gmail.com>
2025-04-03 19:05:46 -0300

    adicionando responsividade ao header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -14,20 +14,7 @@ const Header: React.FC = () => {
       <nav className="header-nav">
         <ul className="header-menu">
           <li><Link to="/">Home</Link></li>
-
-          <li className="brands-dropdown">
-            <span className="brands-link" onClick={toggleBrandsMenu}>Marcas ▾</span>
-            {isBrandsOpen && (
-              <ul className="brands-submenu">
-                <li><Link to="/brands/adidas">Adidas</Link></li>
-                <li><Link to="/brands/new-balance">New Balance</Link></li>
-                <li><Link to="/brands/nike">Nike</Link></li>
-                <li><Link to="/brands/puma">Puma</Link></li>
-                <li><Link to="/brands/all">Todas Chuteiras</Link></li>
-              </ul>
-            )}
-          </li>
-
+          <li><Link to="/catalog">Catálogo</Link></li>
           <li><Link to="/about-us">Sobre nós</Link></li>
           <li><Link to="/faq">FAQ</Link></li>
         </ul>
