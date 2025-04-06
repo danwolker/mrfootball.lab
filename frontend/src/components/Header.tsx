bc1f128def120d46f967871a9efb01a94be0123b
romeu <romeufftt@gmail.com>
2025-04-06 18:49:20 -0300

    retirando rolagem lateral

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,8 @@
-import React, { useState } from "react";
+
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
 const Header: React.FC = () => {
-  const [isBrandsOpen, setIsBrandsOpen] = useState(false);
-
-  const toggleBrandsMenu = () => {
-    setIsBrandsOpen(!isBrandsOpen);
-  };
 
   return (
     <header className="header">
